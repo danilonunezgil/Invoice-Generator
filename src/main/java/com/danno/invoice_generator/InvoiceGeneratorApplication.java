c8ba36dc53f4821c7d585181d303e8c0191f099c
danilonunezgil <danilo.gil98@gmail.com>
2026-07-01 21:33:50 -0500

    🛠️Maint(project skeleton): project structure creation

---
+++ src/main/java/com/danno/invoice_generator/InvoiceGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.danno.invoice_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceGeneratorApplication.class, args);
+	}
+
+}
